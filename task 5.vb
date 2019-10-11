a41f1d1dd585a9e22181eda6bb04dc4c3195d3e2
Aaqib Mehmood <56450241+Aaqib007@users.noreply.github.com>
2019-10-12 00:35:19 +0500

    Add files via upload

---
+++ task 5.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str As String = ""
+        Dim chr1 As Char = ""
+        Dim range As Integer = 0
+        Dim invstr As String = ""
+
+        Console.Write("enter the string that needs to be inversed: ")
+        str = Console.ReadLine
+
+        For range = Len(str) To 1 Step -1
+            chr1 = Mid(str, range, 1)
+            invstr = invstr & chr1
+        Next
+        Console.WriteLine("The inverse of the string is : " & invstr)
+        Console.ReadKey()
+    End Sub
+
+End Module
