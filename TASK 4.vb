c87cf4745f4fa97510a31d777e574152c53b8278
Aaqib Mehmood <56450241+Aaqib007@users.noreply.github.com>
2019-10-12 21:24:53 +0500

    Add files via upload

---
+++ TASK 4.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str As String = ""
+        Dim newstr As String = ""
+        Dim chr1 As Char = ""
+        Dim loop1 As Integer = 0
+        Dim Rchr As String = ""
+
+        Console.Write("Enter the string : ")
+        str = Console.ReadLine
+        
+        Console.Write("Enter the character to be removed : ")
+        Rchr = Console.ReadLine
+
+        For loop1 = 1 To Len(str)
+            chr1 = Mid(str, loop1, 1)
+            If chr1 <> Rchr Then
+                newstr = newstr & chr1
+            End If
+        Next
+        Console.WriteLine("New string with character truncated is " & NewStr)
+        Console.ReadKey()
+    End Sub
+
+End Module
